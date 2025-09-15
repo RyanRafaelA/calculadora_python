d3dd2765341fe4fb0e64e17eec134387f3770f99
RyanRafaelA <ryrafael461@gmail.com>
2025-09-15 15:44:25 -0300

    Criando a janela principal

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import sys
+
+from PySide6.QtWidgets import QApplication, QLabel
+
+from main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+
+    label1 = QLabel("O meu texto")
+    label1.setStyleSheet("font-size: 50px;")
+    window.v_layout.addWidget(label1)
+
+    window.adjustSize()
+
+    window.show()
+    app.exec()
